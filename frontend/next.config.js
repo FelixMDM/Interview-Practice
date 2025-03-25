e64d26e70a04845527c00f36d9f2c90ae8fa1370
felix diaz <felixmdm321@gmail.com>
2025-03-24 21:25:34 -0700

    tailwind

---
+++ frontend/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      loader: "akamai",
+      path: "",
+      unoptimized: true,
+    },
+    basePath: "",
+    assetPrefix: "",
+  };
+  
+  module.exports = nextConfig;
+  
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3B82F6',
+        secondary: '#6B7280',
+      },
+    },
+  },
+  plugins: [],
+} 
